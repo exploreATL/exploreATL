d3693b3439dbe141d690177221c9a812ac1f072a
yzhou43 <77255875+yzhou43@users.noreply.github.com>
2021-11-07 23:07:53 -0500

    get geometry of input places

--- near_places.py
+++ near_places.py
@@ -7,9 +7,14 @@ load_dotenv(find_dotenv())
 # Get the Google Map API key
 API_KEY = os.getenv("API_KEY")
 
+atl_locations = ['downtown','Buckhead','Sandy Springs','Johns Creek','Norcross']
+
 # Get the lat and lng of input places
 # If places is limited, the geometry can be hard coded
-def getGeometry():
+# Input of function is the name of places
+def getGeometry(atl_location):
+    atl_location = atl_location + ' atlanta'
+    atl_location = atl_location.replace(' ','%20')
     url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=downtown%20atlanta&inputtype=textquery&fields=geometry&key={API_KEY}"
 
     payload = {}
@@ -26,17 +31,20 @@ def getGeometry():
     return lat, lng
 
 # Use the Nearby Search Request to find near places
+# Currently hardcode the search type as restaurant
 def getNearPlace():
-    lat, lng = getGeometry()
+    lat, lng = getGeometry(atl_locations[0])
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat}%2C{lng}&radius=1000&type=restaurant&key={API_KEY}"
 
     payload={}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload).json()
+    #
     places = response['results'][0:5]
-    print(places)
+    print(places[0])
 
 getNearPlace()
+
 
 
