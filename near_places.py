1cd1f64ef4ed8131e476f05664e6ced0c08696c5
yzhou43 <77255875+yzhou43@users.noreply.github.com>
2021-11-09 14:57:58 -0500

    fix bugs in function

--- near_places.py
+++ near_places.py
@@ -7,16 +7,30 @@ load_dotenv(find_dotenv())
 # Get the Google Map API key
 API_KEY = os.getenv("API_KEY")
 
-atl_locations = ['downtown','Buckhead','Sandy Springs','Johns Creek','Norcross']
-location_types = ['restaurant','museum','park','university','store','city_hall','amusement_park','library']
+atl_locations = [
+    "downtown atlanta",
+    "buckhead atlanta",
+    "sandy springs",
+    "johns creek",
+    "norcross atlanta",
+]
+location_types = [
+    "restaurant",
+    "museum",
+    "park",
+    "university",
+    "store",
+    "city_hall",
+    "amusement_park",
+    "library",
+]
 
 # Get the lat and lng of input places
 # If places is limited, the geometry can be hard coded
 # Input of function is the name of places
 def getGeometry(atl_location):
-    atl_location = atl_location + ' atlanta'
-    atl_location = atl_location.replace(' ','%20')
-    url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=downtown%20atlanta&inputtype=textquery&fields=geometry&key={API_KEY}"
+    atl_location = atl_location.replace(" ", "%20")
+    url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={atl_location}&inputtype=textquery&fields=geometry&key={API_KEY}"
 
     payload = {}
     headers = {}
@@ -24,34 +38,34 @@ def getGeometry(atl_location):
     response = requests.request("GET", url, headers=headers, data=payload).json()
     # print(response)
     try:
-        geometry = response['candidates'][0]['geometry']['location']
+        geometry = response["candidates"][0]["geometry"]["location"]
     except:
         pass
-    lat = geometry['lat']
-    lng = geometry['lng']
+    lat = geometry["lat"]
+    lng = geometry["lng"]
     return lat, lng
+
 
 # Use the Nearby Search Request to find near places
 # Input is the type of nearby locations
 def getNearPlace(lat, lng, location_type):
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat}%2C{lng}&radius=1000&type={location_type}&key={API_KEY}"
 
-    payload={}
+    payload = {}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload).json()
     # Get the frist five elements of return locations
     try:
-        if len(response['results']) > 5:
-            places = response['results'][0:5]
+        if len(response["results"]) > 5:
+            places = response["results"][0:5]
         else:
-            places = response['results']
+            places = response["results"]
     except:
         pass
     print(places[0])
 
-lat, lng = getGeometry(atl_locations[0])
-getNearPlace(lat, lng, location_types[0])
 
-
+lat, lng = getGeometry(atl_locations[2])
+getNearPlace(lat, lng, location_types[3])
 
