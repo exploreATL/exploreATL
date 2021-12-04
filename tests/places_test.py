86098c0bc65d799ae6da06624b75c17cab7da49f
Michael Roussell <55032397+Mroussell@users.noreply.github.com>
2021-12-03 20:03:22 -0500

    add mocked and unmocked server and client unit tests

--- tests/places_test.py
+++ tests/places_test.py
@@ -4,6 +4,9 @@ from near_places import NearPlaces
 class TestUserInsert(unittest.TestCase):
 
 	def test1_call(self):
+		"""
+		Test Size of outcome for API call and return. 1
+		"""
 		location = "midtown atlanta"
 		loc_type = "restaurant"
 		actual = NearPlaces.getNearPlace(
@@ -15,6 +18,9 @@ class TestUserInsert(unittest.TestCase):
 
 
 	def test2_call(self):
+		"""
+		Test Size of outcome for API call and return. 2
+		"""
 		location = "downtown atlanta"
 		loc_type = "park"
 		actual = NearPlaces.getNearPlace(
@@ -26,6 +32,9 @@ class TestUserInsert(unittest.TestCase):
 
 
 	def test3_call(self):
+		"""
+		Test To insure that 
+		"""
 		location = "midtown atlanta"
 		loc_type = "restaurant"
 		actual = NearPlaces.getNearPlace(
@@ -37,15 +46,19 @@ class TestUserInsert(unittest.TestCase):
 
 
 	def test4_call(self):
+		"""
+		Check for exact match when using API call.
+		"""
 		location = "downtown atlanta"
 		loc_type = "park"
 		actual = NearPlaces.getNearPlace(
             location,
             loc_type,
         )
-		expected = ["place1", "place2", "place3"]
+		expected = [{'name': 'Walton Spring Park', 'photo_reference': 'http://static.panoramio.com/photos/large/62246010.jpg'}, {'name': 'International Peace Fountain', 'photo_reference': 'http://www.soulofamerica.com/soagalleries/atl/genattr/Atl-Woodruff_Intl_Peace_Fount.jpg'}, {'name': 'John Calhoun Park', 'photo_reference': 'https://66.media.tumblr.com/tumblr_m22qt4PW9Y1qc63pwo2_500.jpg'}]
 		self.assertNotEqual(actual, expected)
 
 
 if __name__ == '__main__':
     unittest.main()
+
